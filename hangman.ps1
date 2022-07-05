822fe68b943b753ba800b70a9d3e715682cd0224
Gabriel Roba <roba.gabriel@outlook.com>
2022-07-05 19:07:02 +0300

    powershell script hangman modifications

--- hangman.ps1
+++ hangman.ps1
@@ -1,4 +1,4 @@
-Write-Output "
+Write-Host "
   ________
   |/     |
   |      O
@@ -9,12 +9,16 @@ Write-Output "
  /|\ 
 / | \ 
 "
+Write-Host "Hangman game!
+The theme of the game are animals.
+You have a selection of 23 animals.
+"
 
 $listOfAnimals = @("crocodile", "anaconda", "hippopotamus", "octopus", "alligator", "anteater", "baboon", "dolphin", "lizard", "peacock", "flamingo", "rooster", "gorilla", "buffalo", "caterpillar", "hamster", "chimpanzee", "cougar", "cheetah", "chicken", "elephant", "rattlesnake", "lobster")
 
 $stringLentgh = $listOfAnimals.Count
 #Write-Host $stringLentgh
-Write-Host "before while" $hangmanWord
+#Write-Host "before while" $hangmanWord
 while ($null -eq $hangmanWord) {
     try {
         [int]$hangmanWord = Read-Host "Please enter the Hangman number that represents a word. The intervar is between 1 and" $listOfAnimals.Count
@@ -24,9 +28,9 @@ while ($null -eq $hangmanWord) {
         Write-Host "The value must be a number from 1 to " $stringLentgh "!"
     }
     
-    if ($hangmanWord -ge $stringLentgh){
-        $hangmanWord = $null
-        Write-Host "in while, if statement, value of hangman " $hangmanWord
+    if ($hangmanWord -gt $stringLentgh){
+        Remove-Variable hangmanWord 
+        #Write-Host "in while, if statement, value of hangman " $hangmanWord
         Write-Host "Wrong value was given!"
         Write-Host "The value must be a number from 1 to " $stringLentgh "!"
     }
@@ -35,5 +39,50 @@ while ($null -eq $hangmanWord) {
 #Write-Host $listOfAnimals.GetType()
 #Write-Host $stringLentgh.GetType()
 #Write-Host $hangmanWord.GetType()
-Write-Host $hangmanWord
-Write-Host $listOfAnimals[$hangmanWord - 1]
+Write-Host "
+The value entred is:" $hangmanWord "
+"
+$animalSelected = $listOfAnimals[$hangmanWord - 1]
+#Write-Host $animalSelected
+
+$hangmanAnimal = $animalSelected.ToCharArray()
+$hangmanAnimalLength = $animalSelected.Length
+#Write-Host "This the word selected" $hangmanAnimal 
+#Write-Host "And this is the length of the string" $hangmanAnimalLength
+$emptyString = @()
+for ($i = 0; $i -lt $hangmanAnimalLength; $i++) {
+    $emptyString += "_"
+}
+Write-Host "Here you go!" 
+Write-Host "
+" $emptyString "
+"
+$countFails = 1
+#Write-Host $countFails.GetType()
+while ($countFails -le 6 ) {
+    while ($animalSelected -notmatch $emptyString) {
+        Write-Host "in second while, " $hangmanAnimal $emptyString
+        try {
+            [char]$hangmanLetter = Read-Host "Please enter a letter from the word"
+        }
+        catch {
+            Write-Host "No value or to many values were given !"
+            Write-Host "The value must be a single letter!"
+        }
+        #Write-Host $hangmanLetter
+        #Write-Host $hangmanLetter.GetType()
+        if ($hangmanLetter -match '[0-9]') {
+            Remove-Variable hangmanLetter
+            Write-Host "The values are from 0 to 9!"
+        }
+        else {
+            Write-Host "The values are chars!"
+            break
+        }
+    }
+    Write-Host "in while" $countFails
+    $countFails++
+}
+
+
+
